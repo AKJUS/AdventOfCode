e0c897733b9205a2cdeeb95812e8c463173769e4
JetDev22 <thomas.roelig@gmail.com>
2024-11-13 13:08:08 +0100

    Finally finised Part 2 of Day 5#

--- 2015/day5.js
+++ 2015/day5.js
@@ -8,7 +8,7 @@ const readFile = function(txt){
 };
 var puzzle = readFile(txt);
 
-// Solution 
+// Solution Part 1
 let niceWordsPart1 = 0;
 let niceWordsPart2 = 0;
 let words = puzzle.split("\n");
@@ -16,11 +16,7 @@ let words = puzzle.split("\n");
 for(i = 0; i < words.length; i++){
   let vowels = 0;
   let doubler = 0;
-  let naughty = false;
-  let doubleApperance = 0;
-  let inBetween = 0;
-  let inBetweenFound = false;
-  // Part 1
+  let naughty = false; 
 
   // Check for vowels and count
   for(x = 0; x < words[i].length; x++){
@@ -53,15 +49,39 @@ for(i = 0; i < words.length; i++){
   if(vowels >= 3 && doubler >= 1 && naughty != true){
     niceWordsPart1++;
   }
-
-  // Part 2 
-  // Check for double appearance without overflow
-
-
-  // Check for same letter with different letter in between 
-
 };
 
+// Solution Part 2 
+// Test Cases [works, works, naughty, naughty]
+let testCases = ["qjhvhtzxzqqjkmpb", "xxyxx", "uurcxstgmygtbstg", "ieodomkazucvgmuy"];
+
+function checkForTriplet (word){
+  let result = 0;
+  for(a = 0; a < word.length; a++){
+    if(word[a] == word[a + 2]){
+      result++;
+    }
+  }
+  return result;
+}
+
+function checkForPair(word){
+  let result = 0;
+  for(b = 0; b < word.length - 1; b++){
+    let checkPair = word[b]+word[b + 1];
+    let seen = word.split(checkPair).length - 1;
+    if(seen >= 2){
+      result++;  
+    }
+  }
+  return result;
+}
+
+for(c = 0; c < words.length; c++){
+  if(checkForTriplet(words[c]) >= 1 && checkForPair(words[c]) >= 2){
+    niceWordsPart2++;
+  }
+}
 
 console.log("Nice words found for Part 1:", niceWordsPart1);
 console.log("Nice words found for Part 2:", niceWordsPart2);
