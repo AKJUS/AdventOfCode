1646865fc377b71f577d6b1c16303cd2571e3ea7
JetDev22 <thomas.roelig@gmail.com>
2025-10-17 18:37:31 +0200

    Started 2016

--- 2015/day9.py
+++ 2015/day9.py
@@ -1,5 +1,14 @@
 # Advent of Code 2015 Day 9
 
+cityNames = ["Faerun", "Norrath", "Tristram",
+             "AlphaCentauri", "Arbre", "Snowdin", "Tambi"]
+route = []
+citiesPairs = []
+
 with open("day9.txt") as puzzle:
     for line in puzzle:
-        print(line)
+        raw = line.strip("\n").split("=")
+        cities = raw[0].split(" to ")
+        cities[1] = cities[1].strip()
+        cities.append(int(raw[1]))
+        citiesPairs.append(cities)
---
+++ 2016/day1.py
@@ -0,0 +1,5 @@
+# Advent of Code 2016 - Day 1
+
+with open("day1.txt") as puzzle:
+    for item in puzzle:
+        print(item)
