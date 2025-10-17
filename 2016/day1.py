82318c00015aebad27027a2f9b0cba22245eedb1
JetDev22 <thomas.roelig@gmail.com>
2025-10-17 20:11:18 +0200

    stuck on day 1

--- 2016/day1.py
+++ 2016/day1.py
@@ -1,5 +1,15 @@
 # Advent of Code 2016 - Day 1
 
+route = []
+test = ["R2", "L3"]
+heading = 0
+position = {"x": 0, "y": 0}
+origin = {"x": 0, "y": 0}
+
 with open("day1.txt") as puzzle:
-    for item in puzzle:
-        print(item)
+    for instructions in puzzle:
+        instructions = instructions.strip("\n")
+        route = instructions.split(", ")
+
+
+print(position)
