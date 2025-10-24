2d4b50df19b791c5d43d745c0e8255173db75430
JetDev22 <thomas.roelig@gmail.com>
2025-10-24 20:05:54 +0200

    updated day 1 to correctly pass part 1

--- 2016/day1.py
+++ 2016/day1.py
@@ -1,15 +1,55 @@
 # Advent of Code 2016 - Day 1
 
 route = []
+visited = []
+firstTwice = []
 test = ["R2", "L3"]
-heading = 0
+lastMove = "N"
 position = {"x": 0, "y": 0}
 origin = {"x": 0, "y": 0}
+blocks = 0
 
 with open("day1.txt") as puzzle:
-    for instructions in puzzle:
-        instructions = instructions.strip("\n")
-        route = instructions.split(", ")
+    instructions = puzzle.read().replace("\n", "")
+    route = instructions.split(", ")
 
 
-print(position)
+# Part 1
+for turn in route:
+    if turn[0] == "R" and lastMove == "N":
+        position["x"] += int(turn.replace("R", ""))
+        visited.append(position.copy())
+        lastMove = "E"
+    elif turn[0] == "L" and lastMove == "N":
+        position["x"] -= int(turn.replace("L", ""))
+        visited.append(position.copy())
+        lastMove = "W"
+    elif turn[0] == "R" and lastMove == "E":
+        position["y"] -= int(turn.replace("R", ""))
+        visited.append(position.copy())
+        lastMove = "S"
+    elif turn[0] == "L" and lastMove == "E":
+        position["y"] += int(turn.replace("L", ""))
+        visited.append(position.copy())
+        lastMove = "N"
+    elif turn[0] == "R" and lastMove == "S":
+        position["x"] -= int(turn.replace("R", ""))
+        visited.append(position.copy())
+        lastMove = "W"
+    elif turn[0] == "L" and lastMove == "S":
+        position["x"] += int(turn.replace("L", ""))
+        visited.append(position.copy())
+        lastMove = "E"
+    elif turn[0] == "R" and lastMove == "W":
+        position["y"] += int(turn.replace("R", ""))
+        visited.append(position.copy())
+        lastMove = "N"
+    elif turn[0] == "L" and lastMove == "W":
+        position["y"] -= int(turn.replace("L", ""))
+        visited.append(position.copy())
+        lastMove = "S"
+
+print(f"Target Coordinates are: {position}")
+
+print(f"Shortest Distance in Blocks = {
+      abs(position["x"]) + abs(position["y"])}")
