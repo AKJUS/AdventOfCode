d287125d8a291ac338ea0cbbc1bffb4f410a9356
JetDev22 <thomas.roelig@gmail.com>
2025-10-25 20:13:07 +0200

    finished day 4 part 2

--- 2016/day4.py
+++ 2016/day4.py
@@ -46,3 +46,26 @@ for crypt in encrypted:
                 idSum += int(id)
 
 print(f"The sum of all valid room IDs = {idSum}")
+
+# Part 2
+# length 26
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+for crypt in encrypted:
+    solution = ""
+    crypt = crypt[:-7]
+    id = int(crypt[-3:])
+    crypt = crypt[:-4].split("-")
+    for word in crypt:
+        for char in word:
+            shift = id%26
+            index = abc.index(char)
+            newIndex = index+shift
+            if newIndex > 25:
+                newIndex = newIndex - 26
+                solution += abc[newIndex]
+            else:
+                solution += abc[newIndex]
+    if "north" in solution:
+        print(f"The ID of the North Pole Objects = {id}")
+
