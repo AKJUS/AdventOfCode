05c643bfa82a7cbd7dfe3a7ea79e545705cefb5e
JetDev22 <thomas.roelig@gmail.com>
2025-10-25 21:29:28 +0200

    finished day 5 part 2

--- 2016/day5.py
+++ 2016/day5.py
@@ -6,7 +6,8 @@ doorID = "ffykfhsq"
 integer = 0
 password = ""
 
-print("Please wait for hashing to complete ...")
+# Part 1
+print("Please wait for Part 1 hashing to complete ...")
 while len(password) != 8:
     door = doorID + str(integer)
     result = hashlib.md5(door.encode())
@@ -16,3 +17,29 @@ while len(password) != 8:
     integer += 1
 
 print(f"The password for Part 1 = {password}")
+
+# Part 2
+integer = 0
+newPassword = ""
+collect = [[], [], [], [], [], [], [], []]
+toFill = 8
+print("Please wait for Part 2 hashing to complete")
+while True:
+    door = doorID + str(integer)
+    result = hashlib.md5(door.encode())
+    zeros = result.hexdigest()
+    if "00000" == zeros[:5]:
+        if zeros[5].isdigit():
+            if int(zeros[5]) >= 0 and int(zeros[5]) < 8:
+                index = int(zeros[5])
+            if len(collect[index]) == 0:
+                collect[index] = str(zeros[6])
+                toFill -= 1
+                if toFill == 0:
+                    break
+    integer += 1
+
+for item in collect:
+    newPassword = newPassword + item[0]
+
+print(f"The password for Part 2 = {newPassword}")
