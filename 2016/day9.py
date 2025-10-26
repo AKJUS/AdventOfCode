ffc5690894ac351068dadcabf2cbfa584828b2c4
JetDev22 <thomas.roelig@gmail.com>
2025-10-26 20:05:34 +0100

    prep for day 9

---
+++ 2016/day9.py
@@ -0,0 +1,22 @@
+# Advent of Code 2016 - Day 9
+
+
+def main():
+    rawString = ""
+    with open("day9.txt") as puzzle:
+        for line in puzzle:
+            rawString += line
+
+    print(rawString)
+
+
+def part1(string):
+    pass
+
+
+def part2(string):
+    pass
+
+
+if __name__ == "__main__":
+    main()
