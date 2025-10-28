05ea47284cb36af6c1cba5c6c2318878dd2f658b
JetDev22 <thomas.roelig@gmail.com>
2025-10-28 16:35:49 +0100

    finished day 1 part 2

--- 2017/day1.py
+++ 2017/day1.py
@@ -2,6 +2,7 @@
 
 def main():
     captcha = []
+    test = [[1],[2],[1], [3], [1], [4], [15]]
 
     with open("day1.txt") as puzzle:
         for input in puzzle:
@@ -10,6 +11,8 @@ def main():
                 captcha.append([int(num)])
 
     print(f"The sum for Part 1 = {part1(captcha)}")
+    print(f"The sum for Part 2 = {part2(captcha)}")
+
 
 
 def part1(list):
@@ -24,8 +27,19 @@ def part1(list):
     return sum
 
 
-def part2():
-    pass
+def part2(list):
+    sum = 0
+    indexJump = int(len(list) / 2)
+    for index in range(len(list)):
+        if index + indexJump > len(list)-1:
+            wrap = index + indexJump - len(list)
+            if list[index][0] == list[wrap][0]:
+                sum += list[index][0]
+        else:
+            if list[index][0] == list[index + indexJump][0]:
+                sum += list[index][0]
+    return sum
+
 
 
 if __name__ == "__main__":
